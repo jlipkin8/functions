a118910bc1415541367c995422ba6674b60d25c0
Josie Lipkin <jlipkin8@gmail.com>
2017-04-28 14:24:22 -0700

    started functions homework

---
+++ practice.py
@@ -0,0 +1,65 @@
+"""
+Complete the code to pass all the test
+"""
+
+
+def greet_user():
+    """Prints out each item in a list.
+
+    >>> greet_user()
+    Welcome
+    """
+
+    # ADD YOUR CODE HERE
+    pass
+
+
+def avg_numbers(num1, num2):
+    """Adds two numbers together and divides sum by 2
+
+    >>> avg_numbers(2, 10)
+    6.0
+    >>> avg_numbers(3, 10)
+    6.5
+    """
+
+    # ADD YOUR CODE HERE
+    pass
+
+
+def print_list(lst):
+    """Prints out each item in a list.
+
+    >>> print_list(["ant", "bee", "caterpillar"])
+    ant
+    bee
+    caterpillar
+    """
+
+    # ADD YOUR CODE HERE
+    pass
+
+
+def calculate_total_bill(bill, tip):
+    """Calculates the total bill with tip
+
+    Multiply the tip and bill and that sum is added to the bill.
+
+    >>> calculate_total_bill(100, .20)
+    120.0
+    >>> calculate_total_bill(200, .25)
+    250.0
+    """
+
+    # ADD YOUR CODE HERE
+    pass
+
+
+
+#####################################################################
+# Please don't touch the code below!
+# This allows us to run the doctests
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "\n*** ALL TESTS PASSED. AWESOME WORK!\n"
